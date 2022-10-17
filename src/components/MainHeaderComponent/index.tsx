654646190adbd28e7254c562b3332f06e938c9c6
pedrokpdo <pedromotoehm@gmail.com>
2022-10-17 16:21:49 -0300

    MainHeaderComponent

---
+++ src/components/MainHeaderComponent/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Image, TouchableOpacity } from 'react-native'
+import { Row } from "../funcionais/RowComponent";
+//@ts-ignore
+import logo from '../../assets/logo.png'
+import { Feather, EvilIcons, AntDesign } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
+import { AvatarComponent } from "../AvatarComponent";
+import { useNavigation } from "@react-navigation/native";
+import { Title } from "../funcionais/TitleComponent";
+
+
+export const MainHeaderComponent = () => {
+    const navigation = useNavigation<any>()
+    return (
+        <View>
+            <Row style={{ alignItems: 'center', justifyContent: 'space-between' }}>
+                <Row style={{ alignItems: 'center' }}>
+                    <TouchableOpacity onPress={() => {navigation.goBack()}}>
+                        <AntDesign name="left" size={24} color={'black'} />
+                    </TouchableOpacity>
+                    <Title style={{ marginLeft: 16 }}>Notificações</Title>
+                </Row>
+                <Row style={{ alignItems: 'center', justifyContent: 'space-around', width: 160 }}>
+                    <Feather name='cast' color='black' size={24} />
+                    <TouchableOpacity onPress={() => { navigation.navigate('NotificationListScreen') }}>
+                        <Ionicons name='notifications-outline' color='black' size={24} />
+                    </TouchableOpacity>
+                    <EvilIcons name='search' color='black' size={24} />
+                    <TouchableOpacity onPress={() => { navigation.navigate('ProfileScreen') }}>
+                        <AvatarComponent />
+                    </TouchableOpacity>
+                </Row>
+            </Row>
+        </View>
+    )
+}
--- src/screens/NotificationListScreen/index.tsx
+++ src/screens/NotificationListScreen/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
+import { Space } from "../../components/funcionais/SpaceComponent";
+import { MainHeaderComponent } from "../../components/MainHeaderComponent";
 
 export const NotificationListScreen = () => {
     return (
         <ContainerComponent>
-            
+            <Space top={2}>
+                <MainHeaderComponent />
+            </Space>
         </ContainerComponent>
     )
 }
