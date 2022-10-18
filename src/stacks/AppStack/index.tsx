1ff25176035b47b8029815417b6abd6cecf07124
pedrokpdo <pedromotoehm@gmail.com>
2022-10-18 12:39:32 -0300

    tabBarStyle

--- src/stacks/AppStack/index.tsx
+++ src/stacks/AppStack/index.tsx
@@ -1,3 +1,4 @@
+import { EvilIcons, Ionicons, MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
 import { HomeStack } from '../HomeStack'
@@ -10,11 +11,46 @@ const { Screen, Navigator } = createBottomTabNavigator()
 export const AppStack = () => {
     return (
 
-        <Navigator screenOptions={{ headerShown: false }}>
-            <Screen name='HomeStack' component={HomeStack} />
-            <Screen name='ShortsStack' component={ShortsStack} />
-            <Screen name='InscribeStack' component={InscribeStack} />
-            <Screen name='LibaryStack' component={LibaryStack} />
+        <Navigator screenOptions={{ headerShown: false, tabBarLabelStyle: { marginBottom: 4 } }} >
+            <Screen
+                name='HomeStack'
+                component={HomeStack}
+                options={{
+                    tabBarIcon: ({ color }) =>
+                        (<Ionicons name='home-outline' size={24} color={color} />),
+                    title: 'Home'
+                }} />
+            <Screen
+                name='ShortsStack'
+                component={ShortsStack}
+                options={{
+                    tabBarIcon: ({ color }) => (<MaterialCommunityIcons name='youtube-tv' size={24} color={color} />),
+                    title: 'Shorts'
+                }} />
+            <Screen
+                name='MoreStack'
+                component={ShortsStack}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Ionicons name='add' size={40} color={color} />
+                    ),
+                    title: ''
+
+                }} />
+            <Screen
+                name='InscribeStack'
+                component={InscribeStack}
+                options={{
+                    tabBarIcon: ({ color }) => (<MaterialCommunityIcons name='youtube-subscription' size={24} color={color} />),
+                    title: 'Inscrições'
+                }} />
+            <Screen
+                name='LibaryStack'
+                component={LibaryStack}
+                options={{
+                    tabBarIcon: ({ color }) => (<SimpleLineIcons name='social-youtube' size={24} color={color} />),
+                    title: 'Biblioteca',
+                }} />
         </Navigator>
     )
 }
