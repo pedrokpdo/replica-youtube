96a6010f7fab3b7b608ef5b913d165169f49bd96
pedrokpdo <pedromotoehm@gmail.com>
2022-10-18 13:26:12 -0300

    MyFilmsScreen

--- src/screens/MyFilmsScreen/index.tsx
+++ src/screens/MyFilmsScreen/index.tsx
@@ -12,8 +12,14 @@ import acredito from '../../assets/acredito.jpg'
 import infiltrado from '../../assets/infiltrado.jpg'
 //@ts-ignore
 import mulan from '../../assets/mulan.jpg'
+//@ts-ignore
+import narnia from '../../assets/narnia.jpg'
+//@ts-ignore
+import senhor from '../../assets/senhor.webp'
+//@ts-ignore
+import harry from '../../assets/harry.webp'
 
-export const MyVideosScreen = () => {
+export const MyFilmsScreen = () => {
     return (
         <ContainerComponent>
             <Space top={2}>
@@ -32,6 +38,25 @@ export const MyVideosScreen = () => {
                     </View>
                     <View style={{ marginRight: 16 }}>
                         <ProductComponent photo={mulan} name='Mulan' describe={'Infantil - 2021'} />
+                    </View>
+                </ScrollView>
+            </Space>
+            <Space top={2}>
+                <Row style={{ alignItems: 'center', justifyContent: 'space-between' }}>
+                    <Title bold>Mais vendidos</Title>
+                    <Title style={{ color: 'rgba(98, 159, 229, 1)' }}>VER MAIS</Title>
+                </Row>
+            </Space>
+            <Space top={2}>
+                <ScrollView horizontal>
+                    <View style={{ marginRight: 16 }}>
+                        <ProductComponent photo={harry} name='Harry Potter' describe={'Drama - 2018'} />
+                    </View>
+                    <View style={{ marginRight: 16 }}>
+                        <ProductComponent photo={narnia} name='Narnia' describe={'Ação - 2022'} />
+                    </View>
+                    <View style={{ marginRight: 16 }}>
+                        <ProductComponent photo={senhor} name='Senhor' describe={'Infantil - 2021'} />
                     </View>
                 </ScrollView>
             </Space>
--- src/stacks/LibaryStack/index.tsx
+++ src/stacks/LibaryStack/index.tsx
@@ -1,8 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import { LibaryScreen } from '../../screens/LibaryScreen'
-import { MyVideosScreen } from '../../screens/MyVideosScreen'
-import { ShortsScreen } from '../../screens/ShortsScreen'
+import { MyFilmsScreen } from '../../screens/MyFilmsScreen'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
@@ -10,7 +9,7 @@ export const LibaryStack = () => {
     return (
         <Navigator screenOptions={{ headerShown: false }}>
             <Screen name='LibaryScreen' component={LibaryScreen} />
-            <Screen name='MyVideosScreen' component={MyVideosScreen} />
+            <Screen name='MyFilmsScreen' component={MyFilmsScreen} />
          {/*    <Screen name='MyFilmsScreen' component={MyFilmsScreen} /> */}
         </Navigator>
     )
