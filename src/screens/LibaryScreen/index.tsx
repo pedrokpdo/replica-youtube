6e75a02e25dc44ee9242e0aeeffea0c0d5b213e6
pedrokpdo <pedromotoehm@gmail.com>
2022-10-18 12:54:15 -0300

    MyVideosStack

--- src/screens/LibaryScreen/index.tsx
+++ src/screens/LibaryScreen/index.tsx
@@ -1,6 +1,7 @@
 import { AntDesign, Entypo, Feather, FontAwesome5, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, TouchableOpacity, View } from "react-native";
 import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
 import { Divider } from "../../components/funcionais/Divider";
 import { Row } from "../../components/funcionais/RowComponent";
@@ -13,6 +14,7 @@ import { LitleVideoComponent } from "../../components/LitleVideoComponent";
 
 
 export const LibaryScreen = () => {
+    const navigation = useNavigation<any>()
     return (
         <ContainerComponent>
             <Space top={-2.5}>
@@ -49,10 +51,14 @@ export const LibaryScreen = () => {
             <Divider />
             <Space>
                 <Row top={1}>
-                    <LinkRowComponent Icon={Entypo} iconName='folder-video' text={'Seus Videos'} />
+                    <TouchableOpacity onPress={() => {navigation.navigate('MyVideosScreen')}}>
+                        <LinkRowComponent Icon={Entypo} iconName='folder-video' text={'Seus Videos'} />
+                    </TouchableOpacity>
                 </Row>
                 <Row top={2}>
-                    <LinkRowComponent Icon={Feather} iconName='film' text={'Seus Filmes'} />
+                    <TouchableOpacity onPress={() => {navigation.navigate('MyFilmsScreen')}}>
+                        <LinkRowComponent Icon={Feather} iconName='film' text={'Seus Filmes'} />
+                    </TouchableOpacity>
                 </Row>
             </Space>
             <Divider top={3} bottom={3} />
--- src/stacks/LibaryStack/index.tsx
+++ src/stacks/LibaryStack/index.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import { LibaryScreen } from '../../screens/LibaryScreen'
+import { MyVideosScreen } from '../../screens/MyVideosScreen'
 import { ShortsScreen } from '../../screens/ShortsScreen'
 
 const { Navigator, Screen } = createNativeStackNavigator()
@@ -9,6 +10,8 @@ export const LibaryStack = () => {
     return (
         <Navigator screenOptions={{ headerShown: false }}>
             <Screen name='LibaryScreen' component={LibaryScreen} />
+            <Screen name='MyVideosScreen' component={MyVideosScreen} />
+         {/*    <Screen name='MyFilmsScreen' component={MyFilmsScreen} /> */}
         </Navigator>
     )
 }
