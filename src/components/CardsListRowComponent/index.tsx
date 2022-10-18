4c8a98d6fb101f54ed05106fcb63c0cf31c63649
pedrokpdo <pedromotoehm@gmail.com>
2022-10-18 10:01:37 -0300

    Ajustes (CardsListRowComponent)

--- src/components/CardsListRowComponent/index.tsx
+++ src/components/CardsListRowComponent/index.tsx
@@ -1,31 +1,26 @@
 import React from "react";
 import { View } from 'react-native'
-import { Divider } from "../funcionais/Divider";
-import { FullCard } from "../funcionais/FullCard";
 import { Row } from "../funcionais/RowComponent";
 import { Text } from "../funcionais/TextComponent";
-import { Ionicons, MaterialIcons } from '@expo/vector-icons'
+import { MaterialIcons } from '@expo/vector-icons'
 
-export const CardsListRowComponent = () => {
+export const CardsListRowComponent = ({ focus, setFocus }) => {
     return (
         <Row>
-            <View style={{ backgroundColor: 'gray', height: 30, paddingHorizontal: 8, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
-                <Row style={{ backgroundColor: 'gray', height: 30, paddingHorizontal: 8, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
-                    <MaterialIcons name='explore' size={24} color='white' />
-                    <Text style={{ color: 'white' }}>Explorar</Text>
-                </Row>
+            <View style={{ backgroundColor: focus === 'explore' ? 'gray' : '#dbdbdb', height: 30, paddingHorizontal: 8, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
+                <MaterialIcons name='explore' size={24} color={focus === 'explore' ? 'white' : 'black'} />
             </View>
-            <View style={{ backgroundColor: 'gray', height: 30, paddingHorizontal: 16, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={{ color: 'white' }}>Explorar</Text>
+            <View style={{ backgroundColor: focus === 'all' ? 'gray' : '#dbdbdb', height: 30, paddingHorizontal: 16, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ color: focus === 'all' ? 'white' : 'black' }}>Tudo</Text>
             </View>
-            <View style={{ backgroundColor: 'gray', height: 30, paddingHorizontal: 16, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={{ color: 'white' }}>Explorar</Text>
+            <View style={{ backgroundColor: focus === 'mixes' ? 'gray' : '#dbdbdb', height: 30, paddingHorizontal: 16, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ color: focus === 'mixes' ? 'white' : 'black' }}>Mixes</Text>
             </View>
-            <View style={{ backgroundColor: 'gray', height: 30, paddingHorizontal: 16, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={{ color: 'white' }}>Explorar</Text>
+            <View style={{ backgroundColor: focus === 'musics' ? 'gray' : '#dbdbdb', height: 30, paddingHorizontal: 16, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ color: focus === 'musics' ? 'white' : 'black' }}>Músicas</Text>
             </View>
-            <View style={{ backgroundColor: 'gray', height: 30, paddingHorizontal: 16, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={{ color: 'white' }}>Explorar</Text>
+            <View style={{ backgroundColor: focus === 'games' ? 'gray' : '#dbdbdb', height: 30, paddingHorizontal: 16, marginRight: 16, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ color: focus === 'games' ? 'white' : 'black' }}>Jogos</Text>
             </View>
         </Row>
     )
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { CardsListRowComponent } from "../../components/CardsListRowComponent";
 import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
@@ -9,22 +9,19 @@ import { VideoComponent } from "../../components/VideoComponent";
 import video from '../../assets/video.jpg'
 //@ts-ignore
 import video2 from '../../assets/video2.jpg'
-//@ts-ignore
-import video3 from '../../assets/video3.webp'
-//@ts-ignore
-import video4 from '../../assets/video4.jpg'
 import { BigDivider } from "../../components/BigDividerComponent";
 import { useNavigation } from "@react-navigation/native";
 
 export const HomeScreen = () => {
+    const [focus, setFocus] = useState<any>('all')
     const navigation = useNavigation<any>()
     return (
         <ContainerComponent>
             <Space top={-3}>
-                <HeaderComponent />
+                <HeaderComponent/>
             </Space>
             <Space top={-3} bottom={2}>
-                <CardsListRowComponent />
+                <CardsListRowComponent focus={focus} setFocus={setFocus}/>
             </Space>
             <TouchableOpacity onPress={() => { navigation.navigate('VideoScreen') }}>
                 <VideoComponent name={'cigana'} photo={video} time='1.15' title={'Video de como editar video'} views='4.500' />
